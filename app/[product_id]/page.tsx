09488f2caa0af47096c72402b4aca3de78a768f7
Seifeldeen Ali <seifali101z@gmail.com>
2025-04-03 13:04:06 +0200

    w

--- app/[product_id]/page.tsx
+++ app/[product_id]/page.tsx
@@ -17,9 +17,9 @@ import { Carousel, CarouselContent, CarouselItem, CarouselNext, CarouselPrevious
 import { useParams } from 'next/navigation';
 import Link from 'next/link';
 
-export const MyRates:any = createContext(null)
 
 
+export let MyRates:any = createContext(null)
 
 function page({params}:any) {
 
@@ -124,12 +124,12 @@ const { t, i18n } = useTranslation();
     <>
     <Toaster />
     <Navbar className="text-black" />
-{/* <MyRates.Provider value={setWindow}>
+<MyRates.Provider value={setWindow}>
 {rate_window ? (
-      // <Rate_window id_product={`${product_id}`} />
+      <Rate_window id_product={`${product_id}`} />
 
 ) : null}
-</MyRates.Provider> */}
+</MyRates.Provider>
     <div className="parent py-[80px] flex justify-center">
         <div className="container flex flex-col items-center max-xl:px-[5px]">
           {/* breadChumb first page */}
