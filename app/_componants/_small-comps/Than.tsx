a49ee07f63d4cc9c84d1458be009042885f41ee3
Seifeldeen Ali <seifali101z@gmail.com>
2025-04-22 11:32:35 +0200

    Update Than.tsx

--- app/_componants/_small-comps/Than.tsx
+++ app/_componants/_small-comps/Than.tsx
@@ -67,9 +67,9 @@ export const AnimatedTestimonials = ({
               ease: "easeInOut",
             }}
           >
-            <h3 className="text-2xl font-bold dark:text-white text-black">
+            <h2 className="text-2xl font-bold dark:text-white text-black">
               {testimonials[active].name}
-            </h3>
+            </h2>
             <p className="text-sm text-gray-500 dark:text-neutral-500">
               {testimonials[active].designation}
             </p>
